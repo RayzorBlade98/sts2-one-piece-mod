12ea2552cc1253cf866751f11d27b948387d87c8
RayzorBlade98 <andre-tobias-schweizer@live.de>
2026-04-02 19:53:57 +0200

    Added wapometal laster card

---
+++ Cards/MunchMunchFruit/WapoMetal/WapoMetalLaser.cs
@@ -0,0 +1,28 @@
+using BaseLib.Utils;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using MegaCrit.Sts2.Core.ValueProps;
+
+namespace RayzorBladeOnePiece.Cards.MunchMunchFruit.WapoMetal;
+
+[Pool(typeof(WapoMetalCardPool))]
+public class WapoMetalLaser() : CustomCard(1, CardType.Attack, CardRarity.Token, TargetType.AnyEnemy)
+{
+    protected override IEnumerable<DynamicVar> CanonicalVars =>
+    [
+        new CalculationBaseVar(0M),
+        new ExtraDamageVar(1M),
+        new CalculatedDamageVar(ValueProp.Move)
+            .WithMultiplier((card, _) => PileType.Deck.GetPile(card.Owner).Cards.Count)
+    ];
+
+    public override IEnumerable<CardKeyword> CanonicalKeywords => [CardKeyword.Exhaust];
+
+    protected override async Task OnPlay(PlayerChoiceContext choiceContext, CardPlay cardPlay)
+    {
+        await CommonActions.CardAttack(this, cardPlay, vfx: "vfx/vfx_attack_blunt").Execute(choiceContext);
+    }
+
+    protected override void OnUpgrade() => EnergyCost.UpgradeBy(-1);
+}
