ba518015269bd99fe9576f5d12a82ca0aa7863ed
RayzorBlade98 <andre-tobias-schweizer@live.de>
2026-04-24 20:12:40 +0200

    Added wapo metal bolas and wapo metal emp card

---
+++ Cards/MunchMunchFruit/WapoMetal/WapoMetalEmp.cs
@@ -0,0 +1,44 @@
+using BaseLib.Utils;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.HoverTips;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using MegaCrit.Sts2.Core.Models.Powers;
+
+namespace RayzorBladeOnePiece.Cards.MunchMunchFruit.WapoMetal;
+
+[Pool(typeof(WapoMetalCardPool))]
+public class WapoMetalEmp() : CustomCard(2, CardType.Skill, CardRarity.Token, TargetType.AllEnemies)
+{
+    private const string AmountKey = "Amount";
+
+    protected override IEnumerable<DynamicVar> CanonicalVars => [new(AmountKey, 5M)];
+
+    public override IEnumerable<CardKeyword> CanonicalKeywords => [CardKeyword.Exhaust];
+
+    protected override IEnumerable<IHoverTip> ExtraHoverTips =>
+    [
+        HoverTipFactory.FromPower<WeakPower>(),
+        HoverTipFactory.FromPower<VulnerablePower>()
+    ];
+
+    protected override async Task OnPlay(PlayerChoiceContext choiceContext, CardPlay cardPlay)
+    {
+        if (CombatState is null)
+        {
+            return;
+        }
+
+        await CreatureCmd.TriggerAnim(Owner.Creature, "Cast", Owner.Character.CastAnimDelay);
+        VfxCmd.PlayOnCreatureCenter(Owner.Creature, "vfx/vfx_flying_slash");
+        await CreatureCmd.TriggerAnim(Owner.Creature, "Cast", Owner.Character.CastAnimDelay);
+        foreach (var enemy in CombatState.HittableEnemies)
+        {
+            await CommonActions.Apply<WeakPower>(enemy, this, DynamicVars[AmountKey].BaseValue);
+            await CommonActions.Apply<VulnerablePower>(enemy, this, DynamicVars[AmountKey].BaseValue);
+        }
+    }
+
+    protected override void OnUpgrade() => EnergyCost.UpgradeBy(-1);
+}
