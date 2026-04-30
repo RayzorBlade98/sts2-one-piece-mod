5e6eea821ac8dfb8c11ff810a9d5490cd7602b78
RayzorBlade98 <andre-tobias-schweizer@live.de>
2026-04-30 18:58:19 +0200

    Added spring snipe card

---
+++ Cards/SpringSpringFruit/SpringSnipe.cs
@@ -0,0 +1,30 @@
+using BaseLib.Extensions;
+using BaseLib.Utils;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.HoverTips;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using MegaCrit.Sts2.Core.ValueProps;
+using RayzorBladeOnePiece.Powers;
+
+namespace RayzorBladeOnePiece.Cards.SpringSpringFruit;
+
+[Pool(typeof(SpringSpringFruitCardPool))]
+public class SpringSnipe() : CustomCard(2, CardType.Attack, CardRarity.Uncommon, TargetType.AnyEnemy)
+{
+    protected override IEnumerable<IHoverTip> ExtraHoverTips => [HoverTipFactory.FromPower<SpringedUpPower>()];
+
+    protected override IEnumerable<DynamicVar> CanonicalVars =>
+    [
+        new DamageVar(20M, ValueProp.Move)
+    ];
+
+    protected override bool IsPlayable => Owner.HasPower<SpringedUpPower>();
+
+    protected override async Task OnPlay(PlayerChoiceContext choiceContext, CardPlay cardPlay)
+    {
+        await CommonActions.CardAttack(this, cardPlay, vfx: "vfx/vfx_attack_blunt").Execute(choiceContext);
+    }
+
+    protected override void OnUpgrade() => DynamicVars.Damage.UpgradeValueBy(6M);
+}
